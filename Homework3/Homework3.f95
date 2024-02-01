25f30ed6664e1300ca8d8b66f85e30ff6d311754
TrapDarkness <trapdarkness04@gmail.com>
2024-02-01 15:36:19 -0500

    Initial try for hw3

--- Homework3/Homework3.f95
+++ Homework3/Homework3.f95
@@ -8,28 +8,37 @@ program Homework3
 
     implicit none
     !declare variables here
-    integer size,i
-    real*4 sum, average
-    real*4, allocatable :: lab2_data(:)
+    integer size,i !iterator and array_size
+    real*4 u_previous, u, q, standard_deviation !variables for keeping track of average, q values, and standard deviation
+    real*4, allocatable :: hw3_data(:) !variable sized array for holding data from the file
  
-    !open file, and read first integer in that file
-    open(42,file='CDS251Lab2Data.txt')
-    read(42,*) size
-    !Allocate an array of that size
-    allocate(lab2_data(size))
-    !In one do loop, read in one number into the array and also sum it up.
-    sum = 0
-    do i = 1, size 
-        read(42,*) lab2_data(i) 
-        sum = sum + lab2_data(i)
-    end do 
-    !After the do loop compute the traditional average by dividing by the number of numbers
-    average = sum / size
-    !Print the average to the screen (with a label).
-    print*, "Average of Data: ", average
-    
-    !The answer should be: -8.2000417
+    !Initialize math variables
+    u_previous = 0.0
+    q = 0.0
 
-    deallocate(lab2_data)
+    !open file, and read first integer in that file
+    open(42,file='hw3data.txt')
+    read(42,*) size
+
+    !Allocate an array of that size
+    allocate(hw3_data(size))
+
+    !Loop for Average and Q calculations
+    do i = 1, size 
+        read(42,*) hw3_data(i) !read in data value
+        u = u_previous + ((hw3_data(i) - u_previous) / float(i)) !update average
+        q = q + (hw3_data(i) - u_previous)*(hw3_data(i) - u) !update q
+    end do
+
+    !Sqrt at end of computation for efficiency
+    standard_deviation = sqrt(q/float(size))
+
+    !Print the average to the screen (with a label) + done statement.
+    print*, "Update Average: ", u, "Standard Deviation: ", standard_deviation
+    print*, "Done!"
+    
+    !clean-up
+    deallocate(hw3_data)
     close(42)
+
 end program Homework3
